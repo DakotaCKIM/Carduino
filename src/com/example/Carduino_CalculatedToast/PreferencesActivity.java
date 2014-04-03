9f603f518e903c4ec8ecd701a9c63e006a4002a3
Dakota the Toast <DakotaCKim@gmail.com>
2014-04-03 11:52:39 -0400

    first

---
+++ src/com/example/Carduino_CalculatedToast/PreferencesActivity.java
@@ -0,0 +1,7 @@
+package com.example.Carduino_CalculatedToast;
+
+/**
+ * Created by student on 4/3/14.
+ */
+public class PreferencesActivity {
+}
---
+++ src/com/example/Carduino_CalculatedToast/PreferencesFragment.java
@@ -0,0 +1,13 @@
+package com.example.Carduino_CalculatedToast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by student on 4/3/14.
+ */
+public class PreferencesFragment extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
