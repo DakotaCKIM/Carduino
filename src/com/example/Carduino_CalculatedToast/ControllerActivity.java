aa1e6f02f6f1dbc0d85759eaec4be13ca386a4cd
muk546 <muk546@me.com>
2014-04-03 11:08:33 -0400

    test

--- src/com/example/Carduino_CalculatedToast/ControllerActivity.java
+++ src/com/example/Carduino_CalculatedToast/ControllerActivity.java
@@ -3,13 +3,13 @@ package com.example.Carduino_CalculatedToast;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+public class ControllerActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_controller);
     }
 }
---
+++ src/com/example/Carduino_CalculatedToast/ControllerFragment.java
@@ -0,0 +1,17 @@
+package com.example.Carduino_CalculatedToast;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by student on 4/1/14.
+ */
+public class ControllerFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_controller, container, false);
+    }
+}
