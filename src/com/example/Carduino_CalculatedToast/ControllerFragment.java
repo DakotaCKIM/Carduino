ca255c09d9e1c09fb48b77c6355aa156b6ad2226
muk546 <muk546@me.com>
2014-04-22 11:46:58 -0400

    finish button

--- src/com/example/Carduino_CalculatedToast/ControllerFragment.java
+++ src/com/example/Carduino_CalculatedToast/ControllerFragment.java
@@ -13,5 +13,10 @@ public class ControllerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_controller, container, false);
+
+
+        view viewTreeRoot = inflater.inflate
+
+
     }
 }
